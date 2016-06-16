65201ca3f16d336f50afda4099495a60c3bc7773
yhira <info@nelog.jp>
2016-06-16 15:30:04 +0900

    wp-load.phpの呼び出しパスにABSPATHを利用するように変更

--- lib/fetch-feedly.php
+++ lib/fetch-feedly.php
@@ -1,6 +1,6 @@
 <?php //feedlyカウントの非同期取得用
 //外部のphpからWordpress のAPIを扱う
-require_once('../../../../wp-load.php');
+require_once(ABSPATH.'/wp-load.php');
 //SNS用カウント取得関数の呼び出し
 require_once('sns.php');
 
