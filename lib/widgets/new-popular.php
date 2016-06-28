7d1415defe7d87a2a2875dc44f91d0f56d98dd43
yhira <info@nelog.jp>
2016-06-28 15:14:02 +0900

    新着・人気記事ウィジェットのnotice対策

--- lib/widgets/new-popular.php
+++ lib/widgets/new-popular.php
@@ -12,18 +12,29 @@ class SimplicityNewPopularWidgetItem extends WP_Widget {
   }
   function widget($args, $instance) {
     extract( $args );
-    $title_new = apply_filters( 'widget_title_new', $instance['title_new'] );
-    $title_popular = apply_filters( 'widget_title_popular', $instance['title_popular'] );
-    //表示数を取得
-    $entry_count = apply_filters( 'widget_entry_count', $instance['entry_count'] );
-    //表示タイプ
-    $entry_type = apply_filters( 'widget_entry_type', $instance['entry_type'] );
-    //固定ページを含める
-    $is_pages_include = apply_filters( 'widget_is_pages_include', $instance['is_pages_include'] );
-    $is_views_visible = apply_filters( 'widget_is_views_visible', $instance['is_views_visible'] );
-    $range = apply_filters( 'range', $instance['range'] );
-    $range_visible = apply_filters( 'range_visible', $instance['range_visible'] );
-    $is_ranking_visible = apply_filters( 'is_ranking_visible', $instance['is_ranking_visible'] );
+    // $title_new = apply_filters( 'widget_title_new', $instance['title_new'] );
+    // $title_popular = apply_filters( 'widget_title_popular', $instance['title_popular'] );
+    // //表示数を取得
+    // $entry_count = apply_filters( 'widget_entry_count', $instance['entry_count'] );
+    // //表示タイプ
+    // $entry_type = apply_filters( 'widget_entry_type', $instance['entry_type'] );
+    // //固定ページを含める
+    // $is_pages_include = apply_filters( 'widget_is_pages_include', $instance['is_pages_include'] );
+    // $is_views_visible = apply_filters( 'widget_is_views_visible', $instance['is_views_visible'] );
+    // $range = apply_filters( 'range', $instance['range'] );
+    // $range_visible = apply_filters( 'range_visible', $instance['range_visible'] );
+    // $is_ranking_visible = apply_filters( 'is_ranking_visible', $instance['is_ranking_visible'] );
+
+    $title_new = apply_filters( 'widget_title_new', empty($instance['title_new']) ? "新着記事" : $instance['title_new'] );
+    $title_popular = apply_filters( 'widget_title_popular', empty($instance['title_popular']) ? "人気記事" : $instance['title_popular']);
+    $entry_count = empty($instance['entry_count']) ? 5 : absint( $instance['entry_count'] );
+    $entry_type = apply_filters( 'widget_entry_type', empty($instance['entry_type']) ? "default" : $instance['entry_type'], $instance );
+    $is_pages_include = apply_filters( 'widget_is_pages_include', empty($instance['is_pages_include']) ? "" : $instance['is_pages_include'], $instance );
+    $is_views_visible = apply_filters( 'widget_is_views_visible', empty($instance['is_views_visible']) ? "" :$instance['is_views_visible'], $instance );
+    $range = apply_filters( 'range', empty($instance['range']) ? "" : $instance['range'], $instance );
+    $range_visible = apply_filters( 'range_visible', empty($instance['range_visible']) ? "" : $instance['range_visible'], $instance );
+    $is_ranking_visible = apply_filters( 'is_ranking_visible', empty($instance['is_ranking_visible']) ? "" : $instance['is_ranking_visible'], $instance );
+
     //表示数をグローバル変数に格納
     //後で使用するテンプレートファイルへの受け渡し
     //表示数が設定されていない時は5にする
