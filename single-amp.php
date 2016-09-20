255c21d59a5b8c8e9e7f1cf9f035b1572ec2f173
yhira <info@nelog.jp>
2016-09-20 20:16:28 +0900

    AMPにスタイルをある程度移植

--- single-amp.php
+++ single-amp.php
@@ -11,6 +11,45 @@
 get_template_part('header-ogp');//Facebook OGP用のタグテンプレート?>
 <?php if ( is_twitter_cards_enable() ) //Twitterカードタグ挿入がオンのとき
 get_template_part('header-twitter-card');//Twitterカード用のタグテンプレート?>
+<script async src="https://cdn.ampproject.org/v0.js"></script>
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "NewsArticle",
+  "mainEntityOfPage":{
+    "@type":"WebPage",
+    "@id":"<?php the_permalink(); ?>" // パーマリンクを取得
+  },
+  "headline": "<?php the_title();?>", // ページタイトルを取得
+  "image": {
+    "@type": "ImageObject",
+    "url": "<?php // アイキャッチ画像URLを取得
+    $image_id = get_post_thumbnail_id();
+    $image_url = wp_get_attachment_image_src($image_id, true);
+    echo $image_url[0];
+    ?>",
+    // "height": 800,
+    // "width": 800
+  },
+  "datePublished": "<?php the_time('Y/m/d') ?>", // 記事投稿時間
+  "dateModified": "<?php the_modified_date('Y/m/d') ?>", // 記事更新時間
+  "author": {
+    "@type": "Person",
+    "name": "<?php the_author_meta('nickname'); ?>" // 投稿者ニックネーム
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "<?php bloginfo('name'); ?>", // サイト名
+    // "logo": {
+    //   "@type": "ImageObject",
+    //   "url": "<?php bloginfo('template_url'); ?>/img/logo.png", // ロゴ画像
+    //   "width": 130,
+    //   "height": 53
+    // }
+  },
+  "description": "<?php echo get_the_description(); ?>…" // 抜粋
+}
+</script>
 <style type="text/css">
 <?php
 if ( WP_Filesystem() ) {//WP_Filesystemの初期化
@@ -19,6 +58,7 @@ if ( WP_Filesystem() ) {//WP_Filesystemの初期化
   $css_file = get_template_directory().'/amp.css';
   $css = $wp_filesystem->get_contents($css_file);//ファイルの読み込み
   echo $css;
+  get_template_part('css-custom');
 }
 ?>
 </style>
@@ -54,45 +94,46 @@ if ( WP_Filesystem() ) {//WP_Filesystemの初期化
 
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header>
-    <h1 class="entry-title">
-      <?php the_title(); //投稿のタイトル?>
-    </h1>
-    <p class="post-meta">
-      <?php get_template_part('datetime') //投稿日と更新日?>
-      <?php if ( is_category_visible() && //カテゴリを表示する場合
-                 get_the_category() ): //投稿ページの場合?>
-      <span class="category"><span class="fa fa-folder fa-fw"></span><?php the_category(', ') ?></span>
-      <?php endif; //is_category_visible?>
-    </p>
+  <article class="article">
+    <header>
+      <h1 class="entry-title">
+        <?php the_title(); //投稿のタイトル?>
+      </h1>
+      <p class="post-meta">
+        <?php get_template_part('datetime') //投稿日と更新日?>
+        <?php if ( is_category_visible() && //カテゴリを表示する場合
+                   get_the_category() ): //投稿ページの場合?>
+        <span class="category"><span class="fa fa-folder fa-fw"></span><?php the_category(', ') ?></span>
+        <?php endif; //is_category_visible?>
+      </p>
 
-    <?php get_template_part('admin-pv');//管理者のみにPV表示?>
+      <?php get_template_part('admin-pv');//管理者のみにPV表示?>
 
-    <?php //if ( is_single() ) get_template_part('ad-top');//記事トップ広告 ?>
+      <?php //if ( is_single() ) get_template_part('ad-top');//記事トップ広告 ?>
 
-    <?php //if ( is_single() ) get_template_part('sns-buttons-top');//タイトル下の小さなシェアボタン?>
-  </header>
+      <?php //if ( is_single() ) get_template_part('sns-buttons-top');//タイトル下の小さなシェアボタン?>
+    </header>
 
-  <?php get_template_part('entry-eye-catch');//アイキャッチ挿入機能?>
+    <?php get_template_part('entry-eye-catch');//アイキャッチ挿入機能?>
 
-  <div id="the-content" class="entry-content">
-  <?php //記事本文の表示
-    the_content( get_theme_text_read_more() ); //デフォルト：続きを読む?>
-  </div>
+    <div id="the-content" class="entry-content">
+    <?php //記事本文の表示
+      the_content( get_theme_text_read_more() ); //デフォルト：続きを読む?>
+    </div>
 
-  <footer>
-    <?php if ( is_single() ) get_template_part('pager-page-links');//ページリンクのページャー?>
+    <footer>
+      <?php if ( is_single() ) get_template_part('pager-page-links');//ページリンクのページャー?>
 
-    <?php //if ( is_single() ) get_template_part('ad-article-footer');?>
+      <?php //if ( is_single() ) get_template_part('ad-article-footer');?>
 
-    <p class="footer-post-meta">
-      <?php if (is_tag_visible()): ?>
-      <span class="post-tag"><?php the_tags('<span class="fa fa-tag fa-fw"></span>',', '); ?></span>
-      <?php endif; ?>
+      <p class="footer-post-meta">
+        <?php if (is_tag_visible()): ?>
+        <span class="post-tag"><?php the_tags('<span class="fa fa-tag fa-fw"></span>',', '); ?></span>
+        <?php endif; ?>
 
-      <?php if ( is_single() ) get_template_part('author-link') //投稿者リンク?>
-    </p>
-  </footer>
+        <?php if ( is_single() ) get_template_part('author-link') //投稿者リンク?>
+      </p>
+    </footer>
   </article><!-- .article -->
 </div><!-- .post -->
 
