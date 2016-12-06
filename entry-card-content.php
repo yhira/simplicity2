e2a00aed1390b5e3ac343edb328015a19e49c876
yhira <info@nelog.jp>
2016-12-06 21:39:53 +0900

    カテゴリ表示のセパレーターにHTML要素を追加

--- entry-card-content.php
+++ entry-card-content.php
@@ -18,7 +18,7 @@ if ( is_list_style_large_cards() ||
 
       <?php if ( is_category_visible() && //カテゴリを表示する場合
                  get_the_category() ): //投稿ページの場合?>
-      <span class="category"><span class="fa fa-folder fa-fw"></span><?php the_category(', ') ?></span>
+      <span class="category"><span class="fa fa-folder fa-fw"></span><?php the_category('<span class="category-separator">, </span>') ?></span>
       <?php endif; //is_category_visible?>
 
       <?php //インデクスにタグを表示したい場合コメントアウト
