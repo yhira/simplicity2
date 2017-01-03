0b3a50961e30c918deadf6ed267f2d366e4dedb1
yhira <info@nelog.jp>
2017-01-03 14:11:20 +0900

    lib/speed-up.phpの追加

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ include 'lib/mobile.php';    //モバイル関係の関数
 include 'lib/image.php';     //画像関係の関数
 include 'lib/comment.php';   //コメント関係の関数
 include 'lib/scripts.php';   //スクリプト関係の関数
+include 'lib/speed-up.php';   //高速化関係の関数
 include 'lib/qtags.php';     //クイックタグ関係の関数
 //CFilteringプラグインとの連携
 if ( version_compare( phpversion(), '5.3', '>=' ) ) {
@@ -1077,45 +1078,6 @@ add_filter('body_class', 'add_category_id_classes_to_body_classes');
 // }
 // add_filter('body_class', 'add_category_slug_classes_to_body_classes');
 
-if ( !function_exists( 'defer_async_scripts' ) ):
-function defer_async_scripts( $tag, $handle, $src ) {
-
-  //var_dump($handle);
-  // The handles of the enqueued scripts we want to defer
-  $async_defer = array(
-    //とりあえず影響が計り知れないのでコメントアウト
-    // 'jquery-core',
-    // 'jquery-migrate',
-  );
-  $async_scripts = array(
-    'holder-js',
-    'comment-reply',
-    'lity-js',
-    'lightbox-js',
-  );
-  $defer_scripts = array(
-    'admin-bar',
-    'simplicity-js',
-    'simplicity-child-js',
-    'jquery-lazyload-js',
-    //'crayon_js',
-
-  );
-    if ( in_array( $handle, $async_defer ) ) {
-        return '<script src="' . $src . '" async defer></script>' . "\n";
-    }
-    if ( in_array( $handle, $defer_scripts ) ) {
-        return '<script src="' . $src . '" defer></script>' . "\n";
-    }
-    if ( in_array( $handle, $async_scripts ) ) {
-        return '<script src="' . $src . '" async></script>' . "\n";
-    }
-
-    return $tag;
-}
-endif;
-add_filter( 'script_loader_tag', 'defer_async_scripts', 10, 3 );
-
 //Wordpress管理画面でJavaScriptファイルも編集できるようにする wp4.4以降
 if ( !function_exists( 'add_js_to_wp_theme_editor_filetypes' ) ):
 function add_js_to_wp_theme_editor_filetypes($default_types){
@@ -1167,19 +1129,3 @@ if (!is_rest_api_enable() && (get_wordpress_version() >= 4.7)) {
 function get_wordpress_version(){
   return floatval(get_bloginfo('version'));
 }
-
-//レンダリングブロックしているJavascriptの読み込みを遅らせる
-if ( !function_exists( 'move_scripts_head_to_footer' ) ):
-function move_scripts_head_to_footer(){
-  //ヘッダーのスクリプトを取り除く
-  remove_action('wp_head', 'wp_print_scripts');
-  remove_action('wp_head', 'wp_print_head_scripts', 9);
-  remove_action('wp_head', 'wp_enqueue_scripts', 1);
-
-  //フッターにスクリプトを移動する
-  add_action('wp_footer', 'wp_print_scripts', 5);
-  add_action('wp_footer', 'wp_print_head_scripts', 5);
-  add_action('wp_footer', 'wp_enqueue_scripts', 5);
-}
-endif;
-add_action( 'wp_enqueue_scripts', 'move_scripts_head_to_footer' );
---
+++ lib/speed-up.php
@@ -0,0 +1,58 @@
+<?php //高速化関連の処理
+
+
+//レンダリングブロックしているJavascriptの読み込みを遅らせる
+if ( !function_exists( 'move_scripts_head_to_footer' ) ):
+function move_scripts_head_to_footer(){
+  //ヘッダーのスクリプトを取り除く
+  remove_action('wp_head', 'wp_print_scripts');
+  remove_action('wp_head', 'wp_print_head_scripts', 9);
+  remove_action('wp_head', 'wp_enqueue_scripts', 1);
+
+  //フッターにスクリプトを移動する
+  add_action('wp_footer', 'wp_print_scripts', 5);
+  add_action('wp_footer', 'wp_print_head_scripts', 5);
+  add_action('wp_footer', 'wp_enqueue_scripts', 5);
+}
+endif;
+add_action( 'wp_enqueue_scripts', 'move_scripts_head_to_footer' );
+
+//スクリプトのasyncやdeferの設定
+if ( !function_exists( 'defer_async_scripts' ) ):
+function defer_async_scripts( $tag, $handle, $src ) {
+
+  //var_dump($handle);
+  // The handles of the enqueued scripts we want to defer
+  $async_defer = array(
+    //とりあえず影響が計り知れないのでコメントアウト
+    // 'jquery-core',
+    // 'jquery-migrate',
+  );
+  $async_scripts = array(
+    'holder-js',
+    'comment-reply',
+    'lity-js',
+    'lightbox-js',
+  );
+  $defer_scripts = array(
+    'admin-bar',
+    'simplicity-js',
+    'simplicity-child-js',
+    'jquery-lazyload-js',
+    //'crayon_js',
+
+  );
+    if ( in_array( $handle, $async_defer ) ) {
+        return '<script src="' . $src . '" async defer></script>' . "\n";
+    }
+    if ( in_array( $handle, $defer_scripts ) ) {
+        return '<script src="' . $src . '" defer></script>' . "\n";
+    }
+    if ( in_array( $handle, $async_scripts ) ) {
+        return '<script src="' . $src . '" async></script>' . "\n";
+    }
+
+    return $tag;
+}
+endif;
+add_filter( 'script_loader_tag', 'defer_async_scripts', 10, 3 );
