2f84a7c4c5c9271901ad0bebcea7bdc3b5018de6
yhira <info@nelog.jp>
2017-02-10 21:00:02 +0900

    Google検索ボックスのSSL対応

--- functions.php
+++ functions.php
@@ -1130,7 +1130,7 @@ function get_wordpress_version(){
   return floatval(get_bloginfo('version'));
 }
 
-//投稿内容をSSL化する
+//投稿内容をSSL対応する
 if ( !function_exists( 'chagne_http_to_https' ) ):
 function chagne_site_url_html_to_https($the_content){
   //httpとhttpsURLの取得
@@ -1144,49 +1144,76 @@ function chagne_site_url_html_to_https($the_content){
   //投稿本文の内部リンクを置換
   $the_content = str_replace($http_url, $https_url, $the_content);
 
-  //AmazonアソシエイトのSSL化
+  //AmazonアソシエイトのSSL対応
   $search  = 'http://ecx.images-amazon.com';
   $replace = 'https://images-fe.ssl-images-amazon.com';
   $the_content = str_replace($search, $replace, $the_content);
+  $search  = 'http://ir-jp.amazon-adsystem.com';
+  $replace = 'https://ir-jp.amazon-adsystem.com';
+  $the_content = str_replace($search, $replace, $the_content);
 
 
-  //バリューコマースのSSL化
+  //バリューコマースのSSL対応
   $search  = 'http://ck.jp.ap.valuecommerce.com';
-  $replace = '//ck.jp.ap.valuecommerce.com';
+  $replace = 'https://ck.jp.ap.valuecommerce.com';
   $the_content = str_replace($search, $replace, $the_content);
   $search  = 'http://ad.jp.ap.valuecommerce.com';
-  $replace = '//ad.jp.ap.valuecommerce.com';
+  $replace = 'https://ad.jp.ap.valuecommerce.com';
   $the_content = str_replace($search, $replace, $the_content);
 
-  //もしもアフィリエイトのSSL化
+  //もしもアフィリエイトのSSL対応
   $search  = 'http://c.af.moshimo.com';
-  $replace = '//af.moshimo.com';
+  $replace = 'https://af.moshimo.com';
   $the_content = str_replace($search, $replace, $the_content);
   $search  = 'http://i.af.moshimo.com';
-  $replace = '//i.moshimo.com';
+  $replace = 'https://i.moshimo.com';
   $the_content = str_replace($search, $replace, $the_content);
   $search  = 'http://image.moshimo.com';
-  $replace = '//image.moshimo.com';
+  $replace = 'https://image.moshimo.com';
   $the_content = str_replace($search, $replace, $the_content);
 
-  //A8.netのSSL化
+  //A8.netのSSL対応
   $search  = 'http://px.a8.net';
   $replace = 'https://px.a8.net';
   $the_content = str_replace($search, $replace, $the_content);
+  $search  = 'http://www14.a8.net/0.gif';
+  $replace = 'https://www14.a8.net/0.gif';
+  $the_content = str_replace($search, $replace, $the_content);
+  $search  = 'http://www16.a8.net/0.gif';
+  $replace = 'https://www16.a8.net/0.gif';
+  $the_content = str_replace($search, $replace, $the_content);
 
-  //アクセストレードのSSL化
+  //アクセストレードのSSL対応
   $search  = 'http://h.accesstrade.net';
   $replace = 'https://h.accesstrade.net';
   $the_content = str_replace($search, $replace, $the_content);
 
-  //はてなブログカードのSSL化
+  //はてなブログカードのSSL対応
   $search  = 'http://hatenablog.com/embed?url=';
   $replace = 'https://hatenablog-parts.com/embed?url=';
   $the_content = str_replace($search, $replace, $the_content);
 
-  //個々に新しい置換条件を追加していく
+  //はてブ数画像のSSL対応
+  $search  = 'http://b.hatena.ne.jp/entry/image/';
+  $replace = 'https://b.hatena.ne.jp/entry/image/';
+  $the_content = str_replace($search, $replace, $the_content);
 
-  // //のSSL化
+  //楽天商品画像のSSL対応
+  $search  = 'http://hbb.afl.rakuten.co.jp';
+  $replace = 'https://hbb.afl.rakuten.co.jp';
+  $the_content = str_replace($search, $replace, $the_content);
+
+  //Google検索ボックスのSSL対応
+  $search  = 'http://www.google.co.jp/cse';
+  $replace = 'https://www.google.co.jp/cse';
+  $the_content = str_replace($search, $replace, $the_content);
+  $search  = 'http://www.google.co.jp/coop/cse/brand';
+  $replace = 'https://www.google.co.jp/coop/cse/brand';
+  $the_content = str_replace($search, $replace, $the_content);
+
+  //ここに新しい置換条件を追加していく
+
+  // //のSSL対応
   // $search  = '';
   // $replace = '';
   // $the_content = str_replace($search, $replace, $the_content);
@@ -1194,4 +1221,8 @@ function chagne_site_url_html_to_https($the_content){
   return $the_content;
 }
 endif;
-//add_filter('the_content', 'chagne_site_url_html_to_https', 1);
+// add_filter('the_content', 'chagne_site_url_html_to_https', 1);
+// add_filter('widget_text', 'chagne_site_url_html_to_https', 1);
+// add_filter('widget_text_pc_text', 'chagne_site_url_html_to_https', 1);
+// add_filter('widget_text_mobile_text', 'chagne_site_url_html_to_https', 1);
+// add_filter('comment_text', 'chagne_site_url_html_to_https', 1);
