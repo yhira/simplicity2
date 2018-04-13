c548a0cd0031c6d66bdd7f5856b01e84e87b04b6
yhira <info@nelog.jp>
2018-04-13 20:14:05 +0900

    Ptengineの計測タグ変更

--- analytics.php
+++ analytics.php
@@ -25,17 +25,16 @@ pageTracker._trackPageview();
 <?php if ( get_ptengin_tracking_id() ): ?>
 <!-- Ptengine -->
 <script type="text/javascript">
-  window._pt_sp_2 = [];
-  _pt_sp_2.push('setAccount,<?php echo get_ptengin_tracking_id(); ?>');
-  var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-  (function() {
-    var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
-    atag.src = _protocol + 'js.ptengine.jp/pta.js';
-    var stag = document.createElement('script'); stag.type = 'text/javascript'; stag.async = true;
-    stag.src = _protocol + 'js.ptengine.jp/pts.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(atag, s);s.parentNode.insertBefore(stag, s);
-  })();
+    window._pt_lt = new Date().getTime();
+    window._pt_sp_2 = [];
+    _pt_sp_2.push('setAccount,<?php echo get_ptengin_tracking_id(); ?>');
+    var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    (function() {
+        var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+        atag.src = _protocol + 'js.ptengine.jp/pta.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(atag, s);
+    })();
 </script>
 <!-- /Ptengine -->
 <?php endif ?>
